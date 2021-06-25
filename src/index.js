021983fa2291d68bc850828b3ce9edb0233cc06d
Olekciiy Anurov-Prihodko <anyrov.prihodko@gmail.com>
2021-06-25 17:03:09 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+'use strict';
 import './sass/main.scss';
+import fetchCountries from './fetchCountries.js';
+var debounce = require('lodash.debounce');
+
+
+
+document.querySelector('input').addEventListener(
+  'input',
+  debounce(() => {
+    console.log(
+      'Input event handler invocation after 300ms of inactivity past burst.',
+    );
+  }, 1000),
+);
